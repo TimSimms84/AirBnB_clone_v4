e99847e25e6e325ad60bd7a3586b57487f3bf50f
Tim Simms <tsimms@gmail.com>
2022-04-10 08:37:01 -0500

    fixed owner in description and changed api address

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -12,7 +12,7 @@ $(document).ready(() => {
     }
     $('.amenities h4').text(amenList.join(', '));
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
@@ -26,35 +26,7 @@ $(document).ready(() => {
     contentType: 'application/json',
     success: function (data) {
       for (const place of data) {
-        const html = `<article>
-<div class="title_box">
-<h2>${place.name}</h2>
-<div class="price_by_night">$${place.price_by_night}</div>
-</div>
-<div class="information">
-<div class="max_guest">${place.max_guest} Guests</div>
-<div class="number_rooms">${place.number_rooms} Bedrooms</div>
-<div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-</div>
-<div class="user">
-</div>
-<div class="description">
-${place.description}
-</div>
-</article>`;
-        $('.places').append(html);
-      }
-    }
-  });
-  $('button').click(function () {
-    $('.places > article').remove();
-    $.ajax({
-      type: 'POST',
-      url: 'http://127.0.0.1:5001/api/v1/places_search/',
-      data: JSON.stringify({ amenities: Object.keys(checkAmen) }),
-      contentType: 'application/json',
-      success: function (data) {
-        for (const place of data) {
+        $.get('http://127.0.0.1:5001/api/v1/users/' + place.user_id, function (usrData) {
           const html = `<article>
   <div class="title_box">
   <h2>${place.name}</h2>
@@ -66,12 +38,46 @@ ${place.description}
   <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
   </div>
   <div class="user">
+  <b>Owner:</b> ${usrData.first_name} ${usrData.last_name}
   </div>
   <div class="description">
   ${place.description}
   </div>
   </article>`;
           $('.places').append(html);
+        });
+      }
+    }
+  });
+  $('button').click(function () {
+    $('.places > article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://127.0.0.1:5001/api/v1/places_search/',
+      data: '{}',
+      contentType: 'application/json',
+      success: function (data) {
+        for (const place of data) {
+          $.get('http://127.0.0.1:5001/api/v1/users/' + place.user_id, function (usrData) {
+            const html = `<article>
+    <div class="title_box">
+    <h2>${place.name}</h2>
+    <div class="price_by_night">$${place.price_by_night}</div>
+    </div>
+    <div class="information">
+    <div class="max_guest">${place.max_guest} Guests</div>
+    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+    </div>
+    <div class="user">
+    <b>Owner:</b> ${usrData.first_name} ${usrData.last_name}
+    </div>
+    <div class="description">
+    ${place.description}
+    </div>
+    </article>`;
+            $('.places').append(html);
+          });
         }
       }
     });
